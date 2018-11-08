59c65b01359e33a08547aa2b513da7b64a30dca8
kyajpauley <kyajmil@gmail.com>
2018-11-08 15:04:20 +0900

    initial commit, example file

---
+++ examples/test_stanford_corenlp.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kyajpauley/stanford-corenlp"
+)
+
+func main() {
+	var (
+		tagger *stanford_corenlp.CoreNLPTagger
+		res    []*stanford_corenlp.Result
+		err    error
+	)
+
+	modelPath := "examples/jar_files/*"
+	propertiesPath := "examples/StanfordCoreNLP-chinese.properties"
+
+	tagger, err = stanford_corenlp.NewCoreNLPTagger(modelPath, propertiesPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res, err = tagger.Tag("我来 到 北京 清华大学")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+
+}
